d0dafe46d72e1690ad5b6516d383404ada12db08
jim0428 <kg128888@gmail.com>
2019-05-05 09:45:48 +0800

    can let person go right and left

--- main.js
+++ main.js
@@ -1,2 +1,84 @@
-var c = document.getElementById("myCanvas");
-var cxt=c.getContext("2d");
+
+function startgame(){ 
+    var c = document.getElementById("myCanvas");
+    var cxt=c.getContext("2d");
+    cxt.fillStyle= "#FF0000";
+    //cxt.fillRect(0,0,150,75);
+    //var person = document.getElementById("person");
+    var personx = 200;
+    var persony = 400;
+    var personright = false;
+    var personleft = false;
+    var count = 5;
+    var enemyx = [0,0,0,0,0,0,0,0,0,0];//1-10隻敵人的x座標
+    var enemyy = [0,0,0,0,0,0,0,0,0,0];//1-10隻敵人的y座標
+    var personimg = document.createElement("img");
+    
+    
+    
+    var pci = 2;
+    document.addEventListener("keydown",keydownn,false);
+    document.addEventListener("keyup",keyupp,false);
+    function keydownn(e){
+        if(e.keyCode == 87){
+            personleft =false;
+        }
+        else if(e.keyCode == 65){
+            
+            personleft = true;
+        }
+        else if(e.keyCode == 83){
+            personleft =false;
+        }
+        else if(e.keyCode == 68){
+           
+            count++;
+            if(count >3){
+                //personimg.src = "images/00"+pci+".png";
+                pci++;
+                if(pci > 3)
+                pci = 1;
+                count = 0;
+            }
+            //console.log(22);
+            personright = true;
+        }
+    }
+    function keyupp(e){
+        //console.log(99);
+        if(e.keyCode == 87){
+            personleft =false;
+        }
+        else if(e.keyCode == 65){
+            personleft =false;
+        }
+        else if(e.keyCode == 83){
+            personleft =false;
+        }
+        else if(e.keyCode == 68){
+            
+            personright = false;
+        }
+    }
+    function draw(){
+        
+        cxt.clearRect(0,0,c.width,c.height);
+        
+        if(personright){ 
+            //console.log(persony);
+            personx += 10;
+        }
+        else if(personleft)
+            personx -= 10;
+       
+            personimg.src = "images/00"+pci+".png";
+       cxt.drawImage(personimg,personx,persony,100,100);
+       //pci++;
+        
+
+        //console.log(personright);
+           
+    }
+    setInterval(draw,30); 
+draw();
+}
